39e9f0e047f64bf086d8b0e556095419f8063691
tiarainsani <tiarainsani1806@gmail.com>
2025-10-27 14:54:18 +0700

    Praktikum 5 Kelas A

---
+++ TiaraAmalia/src/pekan5/nestedFor1.java
@@ -0,0 +1,16 @@
+package pekan5;
+
+public class nestedFor1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i<=5; i++) {
+			for (int j=1; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			//to end the line
+		}
+
+	}
+
+}
